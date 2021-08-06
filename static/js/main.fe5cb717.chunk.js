acba091d5e7dcdc338aca3e1d971193c91b965e7
Shine_Lee <wangyi_lixian@163.com>
2021-08-06 10:40:33 +0800

    Updates

--- static/js/main.fe5cb717.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpshine-web"]=this["webpackJsonpshine-web"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);n(1);var s=n(3),r=n.n(s),c=n(0);var i=function(){return Object(c.jsx)("div",{className:"App",children:"another-test"})};r.a.render(Object(c.jsx)(i,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.fe5cb717.chunk.js.map
